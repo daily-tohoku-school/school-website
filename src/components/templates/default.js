38a987715564595ac25112bbf81d5ae9d1ed4a31
macbook <macbook@example.com>
2021-11-15 14:29:35 +0900

    Delete some line from default.js

--- src/components/templates/default.js
+++ src/components/templates/default.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '../organisms/AppBar';
-// import Hero from '../organisms/Hero';
-import Heading from '../molecules/Heading';
-import SummaryList from '../organisms/SummaryList';
-import TeacherList from '../organisms/TeacherList';
-import Reservation from '../organisms/Reservation';
 import Footer from '../organisms/Footer';
 
 const useStyles = makeStyles({
@@ -28,26 +23,8 @@ export default function Default(props) {
         blog={props.blog}
       />
 
-      {/* <Hero
-        image={props.image}
-        imageText={props.imageText}
-        title={props.title}
-        description={props.description}
-        linkText={props.linkText}
-      /> */}
-
-      {/* <Heading heading={props.heading1} />
-      <SummaryList />
-
-      <Heading heading={props.heading2} />
-      <TeacherList />
-
-      <Reservation reservationTitle={props.reservationTitle} reservationButton={props.reservationButton} /> */}
-
-
       <iframe src="https://select-type.com/rsv/?id=Azh6XvffLto&w_flg=1" width="100%" height="800" frameBorder="0">
       </iframe>
-
 
       <Footer />
 
