a6cd3dbd9110980a5d56ce1b23fe77409ca13c61
macbook <macbook@example.com>
2021-11-15 13:57:20 +0900

    Modify Heading.js

--- src/components/molecules/Heading.js
+++ src/components/molecules/Heading.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
-// import Typography from '@material-ui/core/Typography';
 
 export default function Heading(props) {
   return (
     <React.Fragment>
       <Container maxWidth="lg" >
         <Box my={1} textAlign="center" fontSize="h5.fontSize">
-
-          {/* <Typography variant="h4" component="h3" align="center">
-            {props.heading}
-          </Typography> */}
 
           {props.heading}
 
